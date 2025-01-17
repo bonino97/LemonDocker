bfbd284f70c20bd0c5791e1167e3bdfc380fa511
bonino97 <juanbonino97@gmail.com>
2025-01-17 12:00:36 +0100

    fix entry point

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,10 +10,10 @@ export PYTHONPATH=$PYTHONPATH:/opt/api
 export FLOWER_UNAUTHENTICATED_API=true
 
 # Start Celery worker with logging to track task progress in real-time
-celery -A celery_config worker --autoscale=2,1 --loglevel=DEBUG
+celery -A celery_config worker --autoscale=2,1 --loglevel=DEBUG &
 
 # Start Flower for Celery monitoring with unauthenticated API access
-celery -A celery_config flower --port=5555 --loglevel=DEBUG
+celery -A celery_config flower --port=5555 --loglevel=DEBUG &
 
 # Start the Flask API server
 python3 /opt/api/server.py
